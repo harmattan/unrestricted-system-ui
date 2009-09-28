7374741434695dff6b2067a954251d24af554647
telletip <telletip@9267c307-a381-4852-a3f6-864dbf6b9c12>
2009-09-28 09:17:01 +0000

    Script to export themes and images in shared directories.

---
+++ set_themes.sh
@@ -0,0 +1,10 @@
+# Copies the style cheets and images in shared directories
+#
+# Run in scratchbox, from directory har-systemui/cellular-ui/trunk/
+#
+mkdir -p /usr/share/sysuid/themes/style/
+mkdir -p /usr/share/sysuid/themes/svg/
+cp src/*.css /usr/share/sysuid/themes/style/
+cp src/*.svg /usr/share/sysuid/themes/svg/
+cp src/unlocksliderwidget/*.css /usr/share/sysuid/themes/style/
+cp src/unlocksliderwidget/*.svg /usr/share/sysuid/themes/svg/
