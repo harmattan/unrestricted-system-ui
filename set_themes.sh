df8d19a60a583d48e83f81f7e9a8e52f88d590f4
mkatil <mkatil@9267c307-a381-4852-a3f6-864dbf6b9c12>
2009-09-30 10:54:48 +0000

    make the script

--- set_themes.sh
+++ set_themes.sh
@@ -10,3 +10,17 @@ cp src/*.css /usr/share/sysuid/themes/style/
 cp src/*.svg /usr/share/sysuid/themes/svg/
 cp src/unlocksliderwidget/*.css /usr/share/sysuid/themes/style/
 cp src/unlocksliderwidget/*.svg /usr/share/sysuid/themes/svg/
+
+
+# Copies the applets
+#
+cp batteryapplet/battery.desktop /usr/lib/duicontrolpanel/
+cp batteryapplet/*.css /usr/share/duicontrolpanel/themes/style/
+cp batteryapplet/lib/libbatteryapplet.so /usr/lib/duicontrolpanel/applets/
+cp displayapplet/display.desktop /usr/lib/duicontrolpanel/
+cp displayapplet/*.css /usr/share/duicontrolpanel/themes/style/
+cp displayapplet/lib/*.so /usr/lib/duicontrolpanel/applets/
+cp networkapplet/*.desktop /usr/lib/duicontrolpanel/
+cp networkapplet/*.css /usr/share/duicontrolpanel/themes/style/
+cp networkapplet/lib/*.so /usr/lib/duicontrolpanel/applets/
+
