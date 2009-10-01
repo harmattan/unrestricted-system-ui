f47fe0df7e5ca0b5c2b55400e08eb43d563879e0
lapinkataja <lapinkataja@9267c307-a381-4852-a3f6-864dbf6b9c12>
2009-10-01 11:55:08 +0000

    studies done

--- studies/cellusettigns/main.cpp
+++ studies/cellusettigns/main.cpp
@@ -4,10 +4,12 @@
 
 #include "main.h"
 
+#include <RadioAccess>
+
 
 TestObj::TestObj(DuiApplicationWindow &w) : QObject() {
     this->win = &w;
-    this->visible = false;
+    this->visible = false;    
 }
 TestObj::~TestObj() { }
 void TestObj::doStuff()
@@ -18,6 +20,73 @@ void TestObj::doStuff()
         this->win->show();
     else
         this->win->hide();
+
+
+    RadioAccess ra;
+    RadioAccess::Mode raMode;
+    ra.setMode(RadioAccess::UnknownMode);
+    raMode = ra.mode();
+    qDebug() << raMode;
+    ra.setMode(RadioAccess::DualMode);
+    raMode = ra.mode();
+    qDebug() << raMode;
+    ra.setMode(RadioAccess::GSMMode);
+    raMode = ra.mode();
+    qDebug() << raMode;
+    ra.setMode(RadioAccess::UMTSMode);
+    raMode = ra.mode();
+    qDebug() << raMode;
+
+    NetworkRegistration nr;
+    NetworkRegistration::Mode nrMode;
+    nr.setMode(NetworkRegistration::UnknownMode);
+    nrMode = nr.mode();
+    qDebug() << nrMode;
+    nr.setMode(NetworkRegistration::Manual);
+    nrMode = nr.mode();
+    qDebug() << nrMode;
+    nr.setMode(NetworkRegistration::Automatic);
+    nrMode = nr.mode();
+    qDebug() << nrMode;
+
+    networkRegistration = new NetworkRegistration();
+    connect(networkRegistration, SIGNAL(availableOperators(bool, const QList<AvailableOperator*> &, const QString &)),
+           this, SLOT(availableNetworksReceived(bool, const QList<AvailableOperator*> &, const QString &)));
+    networkRegistration->queryAvailableOperators();
+
+
+}
+
+void TestObj::availableNetworksReceived(bool success, const QList<AvailableOperator*> &operators, const QString &reason)
+{
+    delete networkRegistration;
+
+    if(!success) {
+        qDebug() << "not succeded because of " << reason;
+        return;
+    }
+
+    for(int i=0; operators.size(); ++i) {
+        if(operators.at(i)->availability() == AvailableOperator::NotAvailable)
+            qDebug() << operators.at(i)->name() << " is not available";
+        if(operators.at(i)->availability() == AvailableOperator::Current)
+            qDebug() << operators.at(i)->name() << " is current operator";
+        if(operators.at(i)->availability() == AvailableOperator::Available)
+            qDebug() << operators.at(i)->name() << " is available";
+    }
+    if(operators.size() > 0) {        
+        connect(operators.at(0), SIGNAL(selectionCompleted(bool,const QString &)), this, SLOT(selectionCompleted(bool, const QString &)));
+        operators.at(0)->select();
+    }
+
+}
+
+void TestObj::selectionCompleted(bool success, const QString &reason)
+{
+    if(success)
+        qDebug() << "Operator selected sucesfully";
+    else
+        qDebug() << "Operator selecttion failed because of " << reason;
 }
 
 int main(int argc, char** argv)
--- studies/cellusettigns/main.h
+++ studies/cellusettigns/main.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 #include <DuiApplicationWindow>
+#include <NetworkRegistration>
+
+using namespace Cellular;
 
 class TestObj : public QObject
 {
@@ -11,12 +14,20 @@ public:
     TestObj(DuiApplicationWindow &win);
     virtual ~TestObj();
 
+private slots:
+    void availableNetworksReceived(bool, const QList<AvailableOperator*> &, const QString &);
+    void selectionCompleted(bool success, const QString &reason);
+
 private:
     bool visible;
     DuiApplicationWindow *win;
+    NetworkRegistration *networkRegistration;    
+
 
 public slots:
     void doStuff();
+
+
 };
 
 #endif // MAIN_H
