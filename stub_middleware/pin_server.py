28ab487df212cb9321221d0a12dfe230265788b4
mkatil <mkatil@9267c307-a381-4852-a3f6-864dbf6b9c12>
2009-12-14 10:38:24 +0000

    launch pin query by another process

--- stub_middleware/pin_server.py
+++ stub_middleware/pin_server.py
@@ -369,9 +369,14 @@ class UserInterface:
 
         launch = gtk.Button('Launch query')
         def cb_launch(widget):
-            obj = bus.get_object(service_name_pin, service_path_pin)
-            query = dbus.Interface(obj, service_if_pin)
-            print 'LaunchPinQuery returns:', query.LaunchPinQuery('PIN')
+            import os
+            os.system('dbus-send --system --print-reply --dest='+
+                      service_name_pin+' '+
+                      service_path_pin+' '+
+                      service_name_pin+".LaunchPinQuery string:'PIN'")
+            #obj = bus.get_object(service_name_pin, service_path_pin)
+            #query = dbus.Interface(obj, service_if_pin)
+            #print 'LaunchPinQuery returns:', query.LaunchPinQuery('PIN')
         launch.connect('clicked', cb_launch)
         frame.add(launch)
 
