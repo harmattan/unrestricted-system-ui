12464a779bc411a5306b748de9ac524dfef11a14
László Pere <pipas@pipas.blumsoft.eu>
2010-01-18 14:24:05 +0100

    Some cleanup.

--- components/profileapplet/profilecontainer.cpp
+++ components/profileapplet/profilecontainer.cpp
@@ -12,43 +12,70 @@
 #include <DuiGridLayoutPolicy>
 #include <QDebug>
 
+#define DEBUG
+#include "../debug.h"
+
 namespace ProfileApplet
 {
-const int sliderMin = 0;
-const int sliderMax = 100;
+    const int sliderMin = 0;
+    const int sliderMax = 100;
 }
 
-ProfileContainer::ProfileContainer(int id, const QString &title, int level, bool vibra, DuiWidget *parent) :
-        DuiContainer(title, parent),
-        slider(NULL),
-        level(-2),
-        profileId(id)
+ProfileContainer::ProfileContainer (
+        int            id, 
+        const QString &title, 
+        int            level, 
+        bool           vibra, 
+        DuiWidget     *parent) :
+    DuiContainer (title, parent),
+    m_Slider (0),
+    m_Img (0),
+    m_Level (-2),
+    m_ProfileId (id)
 {
     qDebug() << Q_FUNC_INFO << title;
 
     if (0 <= level) {
-        slider = new DuiSlider(0, "continuous");
-        slider->setOrientation(Qt::Horizontal);
-        slider->setRange(ProfileApplet::sliderMin, ProfileApplet::sliderMax);
-        slider->setThumbLabelVisible(false);
-        setLevel(level);
-        connect(slider, SIGNAL(valueChanged(int)), this, SIGNAL(sliderValueChanged(int)));
+        /*
+         * Had to remove the 'continuous' for it ruined the theming of the
+         * slider, the height become huge.
+         */
+        m_Slider = new DuiSlider (0/*, "continuous"*/);
+        m_Slider->setOrientation (Qt::Horizontal);
+        m_Slider->setRange (ProfileApplet::sliderMin, ProfileApplet::sliderMax);
+        #if 0
+        /*
+         * Ok, this worth a try, but did not work. Maybe we should try it again,
+         * with some other version of the libdui.
+         */
+        m_Slider->setMinLabelVisible (true);
+        m_Slider->setMinLabelIconID ("icon-m-volume");
+        #endif
+
+        setLevel (level);
+        connect (m_Slider, SIGNAL (valueChanged(int)), 
+                this, SIGNAL (sliderValueChanged(int)));
+        connect (m_Slider, SIGNAL (valueChanged(int)), 
+                this, SLOT (slotSliderValueChanged(int)));
     }
 
     //% "Vibration"
-    button = new DuiButton (qtTrId ("qtn_prof_vibration"));
-    button->setCheckable(true);
-    setVibration(vibra);
-    connect(button, SIGNAL(toggled(bool)), this, SIGNAL(vibrationChanged(bool)));
+    m_Button = new DuiButton (qtTrId ("qtn_prof_vibration"));
+    m_Button->setCheckable (true);
+    setVibration (vibra);
+    connect (m_Button, SIGNAL(toggled(bool)), 
+            this, SIGNAL(vibrationChanged(bool)));
 
-    setLayout();
+    setLayout ();
 }
 
 ProfileContainer::~ProfileContainer()
 {
 }
 
-void ProfileContainer::setLayout()
+
+void 
+ProfileContainer::setLayout ()
 {
     DuiLayout *layout = new DuiLayout();
 
@@ -59,63 +86,89 @@ void ProfileContainer::setLayout()
     layout->setPortraitPolicy(portraitPolicy); // ownership transferred
 
 
-    DuiImage* img = NULL;
     QGraphicsLayoutItem* item = NULL;
 
-    if (slider) {
-        img = new DuiImage ("icon-m-volume");
-        item = slider;
-        qDebug() << Q_FUNC_INFO << "slider height" << slider->size().height();
-        slider->setMaximumHeight(32);
+    /*
+     * FIXME: Ok, but what happens when we change the 'mute' state?
+     */
+    if (m_Slider) {
+        m_Img = new DuiImage ("icon-m-volume");
+        item = m_Slider;
     } else {
-        img = new DuiImage("icon-m-volume-off");
+        m_Img = new DuiImage ("icon-m-volume-off");
         //% "No ringing"
         item = new DuiLabel (qtTrId ("qtn_prof_noringing"));
     }
-    img->setObjectName("speakerIcon");
+    m_Img->setObjectName ("speakerIcon");
 
-    landscapePolicy->setColumnFixedWidth(0, img->imageSize().width());
-    portraitPolicy->setColumnFixedWidth(0, img->imageSize().width());
-    landscapePolicy->setRowFixedHeight(0, img->imageSize().height());
-    portraitPolicy->setRowFixedHeight(0, img->imageSize().height());
-
-    // set objects
-    landscapePolicy->addItemAtPosition(img, 0, 0);
-    portraitPolicy->addItemAtPosition(img, 0, 0);
-    landscapePolicy->addItemAtPosition(item, 0, 1);
-    portraitPolicy->addItemAtPosition(item, 0, 1);
-    landscapePolicy->addItemAtPosition(button, 1, 0, 1, 2);
-    portraitPolicy->addItemAtPosition(button, 1, 0, 1, 2);
-
-    landscapePolicy->setSpacing(5);
-    portraitPolicy->setSpacing(5);
-
+    /*
+     * Adding the widgets to the proper places.
+     */
+    landscapePolicy->addItemAtPosition (m_Img, 0, 0);
+    portraitPolicy->addItemAtPosition (m_Img, 0, 0);
+    landscapePolicy->addItemAtPosition (item, 0, 1);
+    portraitPolicy->addItemAtPosition (item, 0, 1);
+    landscapePolicy->addItemAtPosition (m_Button, 1, 0, 1, 2);
+    portraitPolicy->addItemAtPosition (m_Button, 1, 0, 1, 2);
+    
     centralWidget()->setLayout(layout);
-
 }
 
-void ProfileContainer::setLevel(int value)
+void 
+ProfileContainer::setLevel (
+        int value)
 {
-    qDebug() << Q_FUNC_INFO << "for" << title() << ":" << value << "(old:" << level << ")";
-    if (!slider || value == level)
+    qDebug() << Q_FUNC_INFO << "for" << title() << ":" << value << 
+        "(old:" << m_Level << ")";
+
+    if (!m_Slider || value == m_Level)
         return;
 
-    level = value;
-    if (ProfileApplet::sliderMin < level && ProfileApplet::sliderMax >= level) {
-        slider->setValue(level);
-        qDebug() << Q_FUNC_INFO << "done:" << slider->value();
+    m_Level = value;
+    if (ProfileApplet::sliderMin < m_Level && 
+            ProfileApplet::sliderMax >= m_Level) {
+        m_Slider->setValue (m_Level);
+        qDebug() << Q_FUNC_INFO << "done:" << m_Slider->value();
     }
-    qDebug() << Q_FUNC_INFO << "done:" << slider->value();
+    qDebug() << Q_FUNC_INFO << "done:" << m_Slider->value();
 }
 
-void ProfileContainer::setVibration(bool enabled)
+/*!
+ * This slot is called when the volume slider value has been changed. The slot
+ * can be changed to modify the icon that shows if the volume is muted. 
+ */
+void
+ProfileContainer::slotSliderValueChanged (
+        int newValue)
+{
+    SYS_DEBUG ("*** newValue = %d", newValue);
+    SYS_DEBUG ("*** m_Level  = %d", m_Level);
+    if (!m_Img)
+        return;
+
+    if (newValue == 0 && m_Level != 0) {
+        SYS_DEBUG ("setIconID (icon-m-volume-off)");
+        m_Img->setImage ("icon-m-volume-off");
+    } else if (newValue != 0 && m_Level == 0) {
+        SYS_DEBUG ("setIconID (icon-m-volume)");
+        m_Img->setImage ("icon-m-volume");
+    }
+}
+
+/*!
+ * This function is called when the vibration enabled/disabled. It will only 
+ * modify the button state.
+ */
+void 
+ProfileContainer::setVibration (
+        bool enabled)
 {
     qDebug() << Q_FUNC_INFO << "for " << title() << ":" << enabled;
-    button->setChecked(enabled);
+    m_Button->setChecked (enabled);
 }
 
 int 
 ProfileContainer::id ()
 {
-    return profileId;
+    return m_ProfileId;
 }
