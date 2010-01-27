0d0dcc865664deb2769603ff46831676537d0c2d
László Pere <pipas@pipas.blumsoft.eu>
2010-01-27 11:27:13 +0100

    Working on tklock.

--- components/sysuid/lockscreenbusinesslogicadaptor.cpp
+++ components/sysuid/lockscreenbusinesslogicadaptor.cpp
@@ -3,19 +3,18 @@
 
 #include "lockscreenbusinesslogicadaptor.h"
 
-LockScreenBusinessLogicAdaptor::LockScreenBusinessLogicAdaptor(
-    QObject *obj, LockScreenBusinessLogic *logic)
-        : QDBusAbstractAdaptor(obj)
+#define DEBUG
+#include "../debug.h"
+
+LockScreenBusinessLogicAdaptor::LockScreenBusinessLogicAdaptor (
+    QObject *obj, 
+    LockScreenBusinessLogic *logic) :
+    QDBusAbstractAdaptor(obj)
 {
     connect (this, SIGNAL(delegateSetMissedEvents(int, int, int, int)),
             logic, SLOT(updateMissedEventAmounts(int, int, int, int)));
 }
 
-#if 0
-LockScreenBusinessLogicAdaptor::~LockScreenBusinessLogicAdaptor()
-{
-}
-#endif
 
 void 
 LockScreenBusinessLogicAdaptor::SetMissedEvents (
@@ -24,5 +23,85 @@ LockScreenBusinessLogicAdaptor::SetMissedEvents (
         int calls, 
         int im)
 {
+    SYS_DEBUG (
+"\n*** emails    = %d"
+"\n*** messages  = %d"
+"\n*** calls     = %d"
+"\n*** im        = %d",
+    emails, messages, calls, im);
+
     emit delegateSetMissedEvents (emails, messages, calls, im);
 }
+
+
+
+int 
+LockScreenBusinessLogicAdaptor::tklock_open (
+        const QString  &a,
+        const QString  &b,
+        const QString  &c,
+        const QString  &d,
+        uint            mode,
+        bool            silent,
+        bool            flicker)
+{
+    SYS_DEBUG (
+"\n*** a         = '%s'"
+"\n*** b         = '%s'"
+"\n*** c         = '%s'"
+"\n*** d         = '%s'"
+"\n*** u         = %u"
+"\n*** silent    = %s"
+"\n*** flicker   = %s",
+            SYS_STR (a),
+            SYS_STR (b),
+            SYS_STR (c),
+            SYS_STR (d),
+            mode,
+            silent ? "true" : "false",
+            flicker ? "true" : "false");
+
+    switch (mode) {
+        case TkLockModeNone:
+            SYS_DEBUG ("*** TkLockModeNone");
+            break;
+        
+        case TkLockModeEnable:
+            SYS_DEBUG ("*** TkLockModeEnable");
+            break;
+
+        case TkLockModeHelp:
+            SYS_DEBUG ("*** TkLockModeHelp");
+            break;
+
+        case TkLockModeSelect:
+            SYS_DEBUG ("*** TkLockModeSelect");
+            break;
+
+        case TkLockModeOneInput:
+            SYS_DEBUG ("*** TkLockModeOneInput");
+            break;
+
+        case TkLockEnableVisual:
+            SYS_DEBUG ("*** TkLockEnableVisual");
+            break;
+
+        default:
+            SYS_WARNING ("*** Unknown mode");
+            break;
+    }
+
+    return (int) TkLockReplyOk;
+}
+
+#if 0
+/*
+ * This is somehow not working. 
+ */
+int
+LockScreenBusinessLogicAdaptor::tklock_close ()
+{
+    SYS_DEBUG ("");
+    return (int) TkLockReplyOk;
+}
+#endif
--- components/sysuid/lockscreenbusinesslogicadaptor.h
+++ components/sysuid/lockscreenbusinesslogicadaptor.h
@@ -13,20 +13,58 @@
 class LockScreenBusinessLogicAdaptor : public QDBusAbstractAdaptor
 {
     Q_OBJECT
+    #if 0
+    // This is what used in MCE.
+    Q_CLASSINFO("D-Bus Interface", "com.nokia.system_ui.request")
+    #else
+    // This is what others want.
     Q_CLASSINFO("D-Bus Interface", "com.nokia.systemui.LockScreen")
+    #endif
 
 public:
     LockScreenBusinessLogicAdaptor (
             QObject *                obj, 
             LockScreenBusinessLogic *delegate);
-    //virtual ~LockScreenBusinessLogicAdaptor();
 
+    /*
+     * I have no information about these, copyed from osso-systemui-tklock.
+     */
+    enum {
+        TkLockReplyFailed = 0,
+        TkLockReplyOk     = 1
+    } TklockReply;
+
+    enum {
+        TkLockModeNone,
+        TkLockModeEnable,
+        TkLockModeHelp,
+        TkLockModeSelect,
+        TkLockModeOneInput,
+        TkLockEnableVisual
+    } TkLockMode;
+    
 public slots:
     Q_NOREPLY void SetMissedEvents (
             int emails, 
             int messages, 
             int calls, 
             int im);
+
+    int tklock_open (
+            const QString  &a,
+            const QString  &b,
+            const QString  &c,
+            const QString  &d,
+            uint       u,
+            bool       b1,
+            bool       b2);
+
+    #if 0
+    /*
+     * This is somehow not working. 
+     */
+    int tklock_close ();
+    #endif
 
 signals:
     void delegateSetMissedEvents (int, int, int, int);
