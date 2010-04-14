c8677e4cb97e0c51e84a68dc2a84b34b362d1b35
Artem Egorkine <ext-artem.egorkine@nokia.com>
2010-04-14 18:14:36 +0300

    Changes: Added a unit test for StatusIndicatorMenuAdaptor
    RevBy: Vesa Halttunen
    Details:
     - test open() method of the adaptor

---
+++ tests/ut_statusindicatormenuadaptor/ut_statusindicatormenuadaptor.cpp
@@ -0,0 +1,67 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** This file is part of systemui.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
+
+#include "ut_statusindicatormenuadaptor.h"
+
+#include <MApplication>
+#include <MApplicationPage>
+#include <MEscapeButtonPanel>
+#include "statusindicatormenuadaptor.h"
+#include "statusindicatormenuwindow_stub.h"
+
+void Ut_StatusIndicatorMenuAdaptor::init()
+{
+    menuWindow = new StatusIndicatorMenuWindow();
+    menuAdaptor = new StatusIndicatorMenuAdaptor(menuWindow);
+    gStatusIndicatorMenuWindowStub->stubReset();
+}
+
+void Ut_StatusIndicatorMenuAdaptor::cleanup()
+{
+    delete menuAdaptor;
+    delete menuWindow;
+}
+
+void Ut_StatusIndicatorMenuAdaptor::initTestCase()
+{
+    // MApplications must be created manually these days due to theme system changes
+    static char *args[] = {(char *)"./ut_statusindicator"};
+    static int argc = sizeof(args) / sizeof(char*);
+
+    app = new MApplication(argc, args);
+
+}
+
+void Ut_StatusIndicatorMenuAdaptor::cleanupTestCase()
+{
+    // Destroy the MApplication
+    delete app;
+}
+
+void Ut_StatusIndicatorMenuAdaptor::testOpenSignal()
+{
+    // When the adaptor open() method is called, the menu window
+    // should be made visible via the makeVisible() method call.
+    QCOMPARE(gStatusIndicatorMenuWindowStub->stubCallCount("makeVisible"), 0);
+    menuAdaptor->open();
+    QCOMPARE(gStatusIndicatorMenuWindowStub->stubCallCount("makeVisible"), 1);
+}
+
+
+QTEST_APPLESS_MAIN(Ut_StatusIndicatorMenuAdaptor)
---
+++ tests/ut_statusindicatormenuadaptor/ut_statusindicatormenuadaptor.pro
@@ -0,0 +1,30 @@
+include(../common_top.pri)
+TARGET = ut_statusindicatormenuadaptor
+
+INCLUDEPATH += $$SRCDIR/statusindicatormenu
+
+#STYLE_HEADERS += $$SRCDIR/statusarea/statusindicatorimagestyle.h \
+#    $$SRCDIR/statusarea/statusindicatorlabelstyle.h
+#MODEL_HEADERS += $$SRCDIR/statusarea/statusindicatormodel.h
+
+# unit test and unit classes
+SOURCES += \
+    $$SRCDIR/statusindicatormenu/statusindicatormenuadaptor.cpp \
+    ut_statusindicatormenuadaptor.cpp
+
+# service classes
+SOURCES += \
+    $$STUBSDIR/stubbase.cpp
+
+# unit test and unit classes
+HEADERS += \
+    $$SRCDIR/statusindicatormenu/statusindicatormenuadaptor.h \
+    $$SRCDIR/statusindicatormenu/statusindicatormenuwindow.h \
+    ut_statusindicatormenuadaptor.h
+
+# service classes
+HEADERS += \
+    $$STUBSDIR/stubbase.h \
+    $$STUBSDIR/statusindicatormenuwindow_stub.h 
+
+include(../common_bot.pri)
