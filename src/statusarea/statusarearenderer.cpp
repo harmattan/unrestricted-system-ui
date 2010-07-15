058e05d991e6fd73a05923087d1fcdcc7b144369
Vesa Halttunen <vesa.halttunen@nokia.com>
2010-07-15 15:26:07 +0300

    Revert "Changes: Removed deprecated status area shared pixmap sharing using temp file"
    This reverts commit 8205a7eedd2ae96ff79113be10315e72b8c53389.

--- src/statusarea/statusarearenderer.cpp
+++ src/statusarea/statusarearenderer.cpp
@@ -75,11 +75,16 @@ bool StatusAreaRenderer::createSharedPixmapHandle()
     statusAreaPixmap = new QPixmap(statusAreaWidth, 2*statusAreaHeight);
     QApplication::syncX();
 
-    if (!statusAreaPixmap->isNull()) {
-        return true;
-    } else {
+    /*!
+     * \deprecated Sharing the pixmap handle using temp file is deprecated. Use dbus interface com.meego.core.MStatusBar for getting the handle.
+     */
+    QFile handleTempFile(QDir::temp().filePath("mstatusbar_pixmap_handle"));
+    if (!handleTempFile.open(QIODevice::WriteOnly)) {
         return false;
     }
+    QDataStream dataStream(&handleTempFile);
+    dataStream << static_cast<quint32> (statusAreaPixmap->handle());
+    return true;
 }
 
 uint StatusAreaRenderer::sharedPixmapHandle()
