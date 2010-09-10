211c32a7cf6449137d78e3530703b3414140bb60
Sachin Kundu <ext-sachin.kundu@nokia.com>
2010-09-10 07:48:23 +0300

    Changes: Fix build break
    RevBy: Trust me

---
+++ src/statusindicatormenu/statusindicatormenuwindowstyle.h
@@ -0,0 +1,39 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** This file is part of systemui.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
+
+#ifndef STATUSINDICATORMENUWINDOWSTYLE_H
+#define STATUSINDICATORMENUWINDOWSTYLE_H
+
+#include <mscenewindowstyle.h>
+
+class StatusIndicatorMenuWindowStyle : public MSceneWindowStyle
+{
+    Q_OBJECT
+    M_STYLE(StatusIndicatorMenuWindowStyle)
+
+    //! controls whether to show a notification area in status indicator menu window
+    M_STYLE_ATTRIBUTE(bool, notificationArea, NotificationArea);
+};
+
+class StatusIndicatorMenuWindowStyleContainer : public MSceneWindowStyleContainer
+{
+    M_STYLE_CONTAINER(StatusIndicatorMenuWindowStyle)
+};
+
+#endif
