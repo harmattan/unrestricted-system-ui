53b342aa53d0ad21ac06bd5a8d0ce93b951d31d7
Vesa Halttunen <vesa.halttunen@nokia.com>
2010-12-03 10:55:46 +0200

    Changes: Fix for the lock screen business logic adaptor unit test
    RevBy: TrustMe

--- tests/ut_lockscreenbusinesslogicadaptor/ut_lockscreenbusinesslogicadaptor.h
+++ tests/ut_lockscreenbusinesslogicadaptor/ut_lockscreenbusinesslogicadaptor.h
@@ -1,49 +1,47 @@
 /****************************************************************************
-**
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (directui@nokia.com)
-**
-** This file is part of systemui.
-**
-** If you have questions regarding the use of this file, please contact
-** Nokia at directui@nokia.com.
-**
-** This library is free software; you can redistribute it and/or
-** modify it under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation
-** and appearing in the file LICENSE.LGPL included in the packaging
-** of this file.
-**
-****************************************************************************/
+ **
+ ** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ ** All rights reserved.
+ ** Contact: Nokia Corporation (directui@nokia.com)
+ **
+ ** This file is part of systemui.
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Nokia at directui@nokia.com.
+ **
+ ** This library is free software; you can redistribute it and/or
+ ** modify it under the terms of the GNU Lesser General Public
+ ** License version 2.1 as published by the Free Software Foundation
+ ** and appearing in the file LICENSE.LGPL included in the packaging
+ ** of this file.
+ **
+ ****************************************************************************/
 #ifndef _UT_LOCKSCREENBUSINESSLOGICADAPTOR_H
 #define _UT_LOCKSCREENBUSINESSLOGICADAPTOR_H
 
 #include <QtTest/QtTest>
 #include <QObject>
 
-#define ADAPTOR_TEST
-
-#include "screenlockbusinesslogicadaptor.h"
-
+class ScreenLockBusinessLogicAdaptor;
 class ScreenLockBusinessLogic;
 
-class Ut_LockScreenBusinessLogicAdaptor : public QObject
-{
-    Q_OBJECT
+class Ut_LockScreenBusinessLogicAdaptor: public QObject {
+Q_OBJECT
 
-    private slots:
-        void initTestCase ();
-        void cleanupTestCase ();
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void init();
+    void cleanup();
 
-        void testTklockOpen ();
-        void testUnlockConfirmed ();
-        void EventEaterIf ();
-        void UnlockScreenIf ();
+    void testTklockOpen();
+    void testUnlockConfirmed();
+    void testEventEaterIf();
+    void testUnlockScreenIf();
 
-    private:
-        ScreenLockBusinessLogic          *m_logic;
-        ScreenLockBusinessLogicAdaptor   *m_subject;
+private:
+    ScreenLockBusinessLogic *m_logic;
+    ScreenLockBusinessLogicAdaptor *m_subject;
 };
 
 #endif
