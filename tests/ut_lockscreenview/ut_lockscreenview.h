69f66fa3a2fd38c581760a4e3d1cff3d923dd699
Pankaj Saharan <pankaj.saharan@nokia.com>
2011-04-18 12:17:22 +0300

    Fixes: NB#247811 - Legal bug in system-ui
    RevBy: TrustMe

--- tests/ut_lockscreenview/ut_lockscreenview.h
+++ tests/ut_lockscreenview/ut_lockscreenview.h
@@ -9,6 +9,11 @@
 ** If you have questions regarding the use of this file, please contact
 ** Nokia at directui@nokia.com.
 **
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
 **
 ****************************************************************************/
 #ifndef UT_LOCKSCREENVIEW_H
--- tests/ut_lockscreenwithpadlockview/ut_lockscreenwithpadlockview.cpp
+++ tests/ut_lockscreenwithpadlockview/ut_lockscreenwithpadlockview.cpp
@@ -9,6 +9,12 @@
 ** If you have questions regarding the use of this file, please contact
 ** Nokia at directui@nokia.com.
 **
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
 ****************************************************************************/
 
 #include <MApplication>
